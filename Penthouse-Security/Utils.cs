f51e1a92d4ba95900f141b79275a2fc62e7291ba
Wojciech <wojciechderwisz@gmail.com>
2019-10-27 21:53:44 +0100

    papudron fix czas zimowy

--- Penthouse-Security/Utils.cs
+++ Penthouse-Security/Utils.cs
@@ -20,7 +20,7 @@ namespace Penthouse_Security
 
         public static int ToGMTHours(int cestHours)
         {
-            return cestHours - 2;
+             return cestHours - 1;
         }
 
         public static TimeSpan GetUptime()
