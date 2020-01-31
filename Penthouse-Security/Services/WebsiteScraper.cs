eae76cf8104f87ee4f649f34b3ceaf3ae4ba6383
Wojciech <wojciech.derwisz@gmail.com>
2020-01-31 21:32:38 +0100

    missing website scrapper source

---
+++ Penthouse-Security/Services/WebsiteScraper.cs
@@ -0,0 +1,46 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Penthouse_Security
+{
+    internal class WebsiteScraper
+    {
+        private string title { get; set; }
+        private string url { get; set; }
+        private string siteUrl = "https://www.worldometers.info/coronavirus/";
+        //public string[] queryTerms { get; } = { "Ocean", "Nature", "Pollution" };
+
+        internal async Task<string> ScrapeWebsite()
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+
+            if (response != null)
+            {
+                var counter = document.All.Where(x => x.ClassName == "maincounter-number");
+                return "Infected: " + counter.First().TextContent;
+            }
+            else
+            {
+                Log.Error("Invalid response.");
+                return "Service unavailable.";
+            }
+        }
+    }
+}
